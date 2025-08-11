d5119332b0c912658df2417485a0e3f04f5239b1
Roman Burov <romanburov89@gmail.com>
2025-08-11 10:53:25 +0300

    Update urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,23 +1,17 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    # Аутентификация через JWT
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # Аутентификация
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('auth/social/', include('dj_rest_auth.social.urls')), 
 
     # Управление пользователями (REST API)
-    path('account/', views.AccountView.as_view(), name='account'),
-    path('account/add/', views.AccountAddView.as_view(), name='account_register'),
-    path('account/update/', views.AccountUpdateView.as_view(), name='account_update'),
-    path('account/delete/', views.AccountDeleteView.as_view(), name='account_delete'),
-
+    path('users/', views.AccountView.as_view(), name='account'),
+    path('users/register/', views.AccountRegisterView.as_view(), name='account_register'),
+    path('users/update/', views.AccountUpdateView.as_view(), name='account_update'),
+    path('users/delete/', views.AccountDeleteView.as_view(), name='account_delete'),
 ]
