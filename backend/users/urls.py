83320afa0138fa58737602183486a1ba2aa3138d
Roman Burov <romanburov89@gmail.com>
2025-08-11 11:36:13 +0300

    Update urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,10 +4,14 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    # Аутентификация и регистрация
+    # Аутентификация и регистрация (Логин и пароль)
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
+
+    # Аутентификация и регистрация (Провайдеров)
     path('auth/social/', include('dj_rest_auth.social.urls')), 
+    path('auth/social/google/', views.GoogleLogin.as_view(), name='google_login'),
+    path('auth/social/yandex/', views.YandexLogin.as_view(), name='yandex_login'),
 
     # Управление пользователями (REST API)
     path('account/', views.AccountView.as_view(), name='account'),
