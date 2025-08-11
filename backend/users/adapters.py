1ac091a75aebdb6ac16d5db0f0b844fee0531751
Roman Burov <romanburov89@gmail.com>
2025-08-11 18:03:04 +0300

    Create adapters.py

---
+++ backend/users/adapters.py
@@ -0,0 +1,10 @@
+from allauth.account.adapter import DefaultAccountAdapter
+
+class CustomAccountAdapter(DefaultAccountAdapter):
+    def save_user(self, request, user, form, commit=True):
+        data = form.cleaned_data
+        user.email = data.get('email')
+        user.set_password(data.get('password1'))
+        if commit:
+            user.save()
+        return user
