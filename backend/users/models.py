6d87a6192c3a772d0e0ce6600a42374a3315b3d1
Roman Burov <romanburov89@gmail.com>
2025-08-11 12:55:54 +0300

    Update models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,56 +1,17 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('Email должен быть указан')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Суперпользователь должен иметь is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Суперпользователь должен иметь is_superuser=True.')
-
-        return self.create_user(email, password, **extra_fields)
-
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    '''Пользователь'''
-
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    email = models.EmailField(
-        verbose_name="Электронная почта",
-        max_length=254,
-        unique=True
-    )
-    first_name = models.CharField(
-        max_length=15,
-        verbose_name="Имя",
-        blank=True,
-        validators=(UnicodeUsernameValidator())
-    )
-    image = models.ImageField(
-        upload_to='users',
-        null=True,
-        blank=True,
-        verbose_name="Изображение"
-    )
-    objects = CustomUserManager()
-
-    class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-
     def __str__(self):
         return self.email
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
