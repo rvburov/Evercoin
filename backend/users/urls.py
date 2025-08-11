2ccead2dc939eae59fe1e45dbe265c099e72484a
Roman Burov <romanburov89@gmail.com>
2025-08-11 11:27:17 +0300

    Update urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
 
     # Управление пользователями (REST API)
     path('account/', views.AccountView.as_view(), name='account'),
-    path('account/register/', views.AccountRegisterView.as_view(), name='account_register'),
     path('account/update/', views.AccountUpdateView.as_view(), name='account_update'),
     path('account/delete/', views.AccountDeleteView.as_view(), name='account_delete'),
 ]
