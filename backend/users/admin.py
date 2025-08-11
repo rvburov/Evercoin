51de34c01f75104b59b2dc4a221e1f7c70919153
Roman Burov <romanburov89@gmail.com>
2025-08-11 09:48:07 +0300

    Update admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,1 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'first_name')
+    fieldsets = (
+        (None, {'fields': ('email', 'first_name', 'password', 'image')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
