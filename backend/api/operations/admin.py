eda69343ccd9db5674eb09d311ae542f82341371
Roman Burov <romanburov89@gmail.com>
2025-08-14 17:55:19 +0300

    Update admin.py

--- backend/api/operations/admin.py
+++ backend/api/operations/admin.py
@@ -1,1 +1,11 @@
+from django.contrib import admin
+from .models import Operation
 
+
+@admin.register(Operation)
+class OperationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'amount', 'type', 'date', 'user', 'wallet')
+    list_filter = ('type', 'date', 'user', 'wallet')
+    search_fields = ('title', 'comment', 'user__email')
+    date_hierarchy = 'date'
+    ordering = ('-date',)
