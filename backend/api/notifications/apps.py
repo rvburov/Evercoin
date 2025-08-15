fd23aab9a6a6444aa1afcabba50dd6436aca084c
Roman Burov <romanburov89@gmail.com>
2025-08-15 14:48:57 +0300

    Create apps.py

---
+++ backend/api/notifications/apps.py
@@ -0,0 +1,7 @@
+# project/backend/api/notifications/apps.py
+from django.apps import AppConfig
+
+
+class NotificationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api.notifications'
