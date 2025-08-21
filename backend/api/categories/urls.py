f02796e6d4514deb25c469bc1c6d29024f34f700
Roman Burov <romanburov89@gmail.com>
2025-08-21 17:54:44 +0300

    Update urls.py

--- backend/api/categories/urls.py
+++ backend/api/categories/urls.py
@@ -1,13 +1,11 @@
 # project/backend/api/categories/urls.py
-from django.urls import path
-from .views import (
-    CategoryListCreateView,
-    CategoryDetailView,
-    category_tree
-)
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet, basename='category')
 
 urlpatterns = [
-    path('categories/', CategoryListCreateView.as_view(), name='category-list'),
-    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
-    path('categories/tree/', category_tree, name='category-tree'),
+    path('api/', include(router.urls)),
 ]
