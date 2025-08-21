d9e6d23561bfb3a63df2be9e47bbf5269c370de6
Roman Burov <romanburov89@gmail.com>
2025-08-21 18:00:54 +0300

    Update urls.py

--- backend/api/operations/urls.py
+++ backend/api/operations/urls.py
@@ -1,22 +1,11 @@
 # project/backend/api/operations/urls.py
-from django.urls import path
-from .views import (
-    OperationListCreateView,
-    OperationDetailView,
-    OperationSummaryView,
-    RecurringOperationListCreateView,
-    RecurringOperationDetailView,
-    RecurringOperationProcessView
-)
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import OperationViewSet
+
+router = DefaultRouter()
+router.register(r'operations', OperationViewSet, basename='operation')
 
 urlpatterns = [
-    # Основные операции
-    path('operations/', OperationListCreateView.as_view(), name='operation-list'),
-    path('operations/<int:pk>/', OperationDetailView.as_view(), name='operation-detail'),
-    path('operations/summary/', OperationSummaryView.as_view(), name='operation-summary'),
-    
-    # Повторяющиеся операции
-    path('recurring-operations/', RecurringOperationListCreateView.as_view(), name='recurring-operation-list'),
-    path('recurring-operations/<int:pk>/', RecurringOperationDetailView.as_view(), name='recurring-operation-detail'),
-    path('recurring-operations/process/', RecurringOperationProcessView.as_view(), name='recurring-operation-process'),
+    path('api/', include(router.urls)),
 ]
