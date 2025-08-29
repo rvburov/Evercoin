22989b747e989b3a5b416cf9d87207596fe84864
Roman Burov <romanburov89@gmail.com>
2025-08-29 17:11:47 +0300

    Update apps.py

--- backend/api/wallets/apps.py
+++ backend/api/wallets/apps.py
@@ -1,7 +1,8 @@
-# project/backend/api/notifications/apps.py
+# evercoin/backend/api/wallets/apps.py
 from django.apps import AppConfig
 
 
 class WalletsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api.wallets'
+    verbose_name = 'Финансовые счета'
